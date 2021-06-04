018bf8e7a4ad0db5e65bfae850cb154138c39889
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2021-06-04 16:41:52 -0400

    Added my first test

--- 2.44.0/test.sh
+++ 2.44.0/test.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo "No tests have been created for this repository so far."
+INPUT=data/file.dot
+OUTPUT=data/truth1.png
+
+alias dot='singularity exec singularity-graphviz-2.44.0.sif dot'
+dot -Tpng $INPUT -o $OUTPUT
+
+if [ -f $OUTPUT ]; then
+        file $OUTPUT
+	exit 0
+else
+	exit 1
+fi
