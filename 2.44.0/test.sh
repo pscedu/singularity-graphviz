61bd327b3904d32e1f24dab7c42fb040622aca6f
icaoberg <icaoberg@andrew.cmu.edu>
2021-06-04 17:00:42 -0400

    Update test.sh

--- 2.44.0/test.sh
+++ 2.44.0/test.sh
@@ -19,10 +19,10 @@ EOF
 INPUT=file.dot
 OUTPUT=truth1.png
 
-ls -lta
-
 chown 1001:116 file.dot
-chown 1001:166 singularity-graphviz-2.44.0.sif
+chown 1001:116 singularity-graphviz-2.44.0.sif
+
+ls -lta
 
 singularity exec singularity-graphviz-2.44.0.sif dot -Tpng "$INPUT" -o "$OUTPUT"
 
