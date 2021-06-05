dd7d79c44bc2dfac660dfe61742b3bc0826e42af
icaoberg <icaoberg@andrew.cmu.edu>
2021-06-04 21:52:46 -0400

    Update test.sh

--- 2.44.0/test.sh
+++ 2.44.0/test.sh
@@ -5,6 +5,10 @@ set -x
 INPUT=./data/file.dot
 OUTPUT=./data/truth1.png
 
+echo "Files in this repository"
+find . -type f
+
+echo "Run graphviz"
 singularity exec singularity-graphviz-2.44.0.sif dot -Tpng "$INPUT" -o "$OUTPUT"
 file $OUTPUT
 
