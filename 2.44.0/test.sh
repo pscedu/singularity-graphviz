6ad152808d5208ebde85278075ad19400f0658ff
icaoberg <icaoberg@andrew.cmu.edu>
2021-07-01 15:33:32 -0400

    Update test.sh

--- 2.44.0/test.sh
+++ 2.44.0/test.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 
 set -x
-
-INPUT=./data/file.dot
-OUTPUT=./data/truth1.png
-
-echo "Files in this repository"
-find . -type f
-
-echo "Run graphviz"
-#singularity exec singularity-graphviz-2.44.0.sif dot -Tpng "$INPUT" -o "$OUTPUT"
-#file $OUTPUT
-
-if [ -f $OUTPUT ]; then
-	exit 0
-else
-	exit 1
-fi
+echo "Nothing to see here..."
