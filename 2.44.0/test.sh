9b5f9f156e7b67ada53eda3527bfd08eed3c8459
Ivan Cao-Berg <icaoberg@psc.edu>
2022-01-25 18:51:22 -0500

    Updated test file

--- 2.44.0/test.sh
+++ 2.44.0/test.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 set -x
-
 shopt -s expand_aliases
 
 DIRECTORY=$(pwd)
 alias dot='singularity exec "$DIRECTORY"/singularity-graphviz-2.44.0.sif dot'
 
-echo 'digraph { a -> b; }' | dot
+echo 'digraph { a -> b; }' > /tmp/file.dot
+dot -Tpng /tmp/file.dot -o output.png
+
+if [ -f output.png ]; then
+	exit 0
+else
+	exit 1
+fi
