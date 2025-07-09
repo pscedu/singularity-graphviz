cb8210f4931e7871aaa582da423ef52cc13aa642
Luis-Rubio <luisjorgerubio@gmail.com>
2025-07-09 14:45:38 -0400

    Update test.sh
    updated version and year

--- 13.1.0/test.sh
+++ 13.1.0/test.sh
@@ -5,6 +5,6 @@ set -x
 shopt -s expand_aliases
 
 DIRECTORY=$(pwd)
-alias dot='./singularity-graphviz-9.0.0.sif dot'
+alias dot='./singularity-graphviz-13.1.0.sif dot'
 
 echo 'digraph { a -> b; }' | dot
